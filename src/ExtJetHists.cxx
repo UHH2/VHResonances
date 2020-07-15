122e6a07b3b078c24b88a1be28e602d59651f78c
tom-so <>
2020-07-15 12:02:40 +0200

    Removed Delta R of jet and MET plot

--- src/ExtJetHists.cxx
+++ src/ExtJetHists.cxx
@@ -195,7 +195,6 @@ void ExtJetHists::book_jetHist(const string & histSuffix, const string & axisSuf
   book_TH1F("deltaR_ele2"+histSuffix,"#Delta R(ele2,"+axisSuffix+")",40, 0, 8.0);
   book_TH1F("deltaphi_ele2"+histSuffix,"#Delta#phi(ele2,"+axisSuffix+")",50,0,M_PI);
   book_TH1F("deltaphi_MET"+histSuffix,"#Delta#phi(E_{T}^{miss},"+axisSuffix+")",50,0,M_PI);
-  book_TH1F("deltaR_MET"+histSuffix,"#Delta R(E_{T}^{miss},"+axisSuffix+")",50,0,M_PI);
   book_TH1F("jetArea"+histSuffix,"jetArea^{"+axisSuffix+"}",150,0,15);
   book_TH1F("jetArea_pt200_300"+histSuffix,"jetArea^{"+axisSuffix+",pt(200,300)}",150,0,15);
   book_TH1F("jetArea_pt300_400"+histSuffix,"jetArea^{"+axisSuffix+",pt(300,400)}",150,0,15);
@@ -302,7 +301,6 @@ void ExtJetHists::fill_jetHist<Jet>(const Event & event, const string& histSuffi
     }
   }
   fill_H1("deltaphi_MET"+histSuffix, deltaPhi(jet, event.met->v4()), weight);
-  fill_H1("deltaR_MET"+histSuffix, deltaR(jet, event.met->v4()), weight);
   fill_H1("jetArea"+histSuffix, jet.jetArea(), weight);
   if (jet.pt()>200 || jet.pt()<300) fill_H1("jetArea_pt200_300"+histSuffix,jet.jetArea(), weight);
   if (jet.pt()>300 || jet.pt()<400) fill_H1("jetArea_pt300_400"+histSuffix,jet.jetArea(), weight);
