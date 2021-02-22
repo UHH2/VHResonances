e72939e60876849ee7768e6932f863b623a8cb00
Andrea Malara <andrea.malara@cern.ch>
2021-02-22 16:32:08 +0100

    Fixed PDF reweighting

--- src/PDFReweightModule.cxx
+++ src/PDFReweightModule.cxx
@@ -70,12 +70,22 @@ protected:
   // Define variables
   std::string NameModule = "PDFReweightModule";
   std::vector<std::string> histogram_tags = { "nocuts", "weights"};
+  std::vector<std::string> Systematics = {"PDF", "NNPDF", "NNPDF31_lo_as_0130", "PDF4LHC15_nnlo_100"};
+
+  int PDF_variations = 100; int pdfindex_shift = 9;
+  std::unordered_map<std::string, std::string> PFDs = {
+    {"NNPDF", "NNPDF31_nnlo_as_0118_nf_4_mc_hessian"},
+    {"NNPDF31_lo_as_0130", "NNPDF31_lo_as_0130"},
+    {"PDF4LHC15_nnlo_100", "PDF4LHC15_nnlo_100"},
+  };
 
   std::unordered_map<std::string, std::string> MS;
   std::unordered_map<std::string, bool> MB;
 
   // Define common modules
   std::unique_ptr<AnalysisModule> PDFReweight_module;
+  std::unordered_map<std::string, std::vector<double> > PDF_weights;
+  std::unordered_map<std::string, std::unique_ptr<PDFWeights> > m_pdfweights;
 
 };
 
@@ -96,12 +106,32 @@ void PDFReweightModule::book_histograms(uhh2::Context& ctx) {
     mytag = "gen_"      + tag; book_HFolder(mytag, new GenMatchHists(ctx,mytag));
     mytag = "nTopJet_"  + tag; book_HFolder(mytag, new ExtJetHists(ctx,mytag, MS["topjetLabel"]));
     mytag = "nJet_"     + tag; book_HFolder(mytag, new ExtJetHists(ctx,mytag, MS["jetLabel"]));
+    for (std::string& syst :  Systematics){
+      for(int i=0; i<PDF_variations; i++){
+        std::string var = to_string(i);
+        mytag = "ZprimeCandidate_"+syst+"_"+var+"_"+tag; book_HFolder(mytag, new HiggsToWWHistsSlim(ctx,mytag));
+      }
+    }
   }
 }
 
 void PDFReweightModule::fill_histograms(uhh2::Event& event, string tag){
   std::vector<string> mytags = {"event_", "gen_", "nTopJet_", "nJet_"};
   for (auto& mytag : mytags) HFolder(mytag+ tag)->fill(event);
+  string mytag;
+  float save_weight = event.weight;
+
+  for (std::pair<std::string, std::string> pdf : PFDs) PDF_weights[pdf.first] = m_pdfweights[pdf.first]->GetWeightList(event);
+
+  for (std::string& syst :  Systematics){
+    for(int i=0; i<PDF_variations; i++){
+      double weightFactor = (syst=="PDF")? event.genInfo->systweights().at(i+pdfindex_shift)/event.genInfo->systweights().at(pdfindex_shift) : PDF_weights[syst][i];
+      event.weight = save_weight*weightFactor;
+      std::string var = to_string(i);
+      mytag = "ZprimeCandidate_"+syst+"_"+var+"_"+tag; HFolder(mytag)->fill(event);
+    }
+  }
+  event.weight = save_weight;
 }
 
 /*
@@ -128,20 +158,20 @@ PDFReweightModule::PDFReweightModule(uhh2::Context& ctx){
   if ((MB["isPuppi"] && MB["isCHS"]) || (MB["isPuppi"] && MB["isHOTVR"]) || (MB["isCHS"] && MB["isHOTVR"]) ) throw std::runtime_error("In "+NameModule+".cxx: Choose exactly one jet collection.");
   if ((MB["muonchannel"] && MB["electronchannel"]) || (MB["muonchannel"] && MB["invisiblechannel"]) || (MB["electronchannel"] && MB["invisiblechannel"])) throw std::runtime_error("In "+NameModule+".cxx: Choose exactly one lepton channel.");
 
+  if (!FindInString("MC_ZprimeToZH", MS["dataset_version"])) throw std::runtime_error("In "+NameModule+".cxx: Not meant to run on not signal samples.");
+
   MS["leptons"] = MB["muonchannel"]? "muons": (MB["electronchannel"]? "electrons": (MB["invisiblechannel"]? "invisible": ""));
 
   MS["jetLabel"]    = MB["isCHS"]? "jets":    (MB["isPuppi"]? "jetsAk4Puppi": (MB["isHOTVR"]? "jetsAk4Puppi": ""));
   MS["topjetLabel"] = MB["isCHS"]? "topjets": (MB["isPuppi"]? "toppuppijets": (MB["isHOTVR"]? "hotvrPuppi": ""));
 
   // Set up histograms:
-
   book_histograms(ctx);
   PrintInputs();
 
   // Set up selections
-
   PDFReweight_module.reset(new PDFReweight(ctx));
-
+  for (std::pair<std::string, std::string> pdf : PFDs) m_pdfweights[pdf.first].reset(new PDFWeights(pdf.second));
 }
 
 
